1c8b8d4b4424895b2f0433ab10b3db44c8674bbb
admin <11@qq.com>
2019-04-19 09:21:02 +0800

    4.19

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -1,7 +1,11 @@
 package com.example.admin.myapplication;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
         import android.widget.ListView;
         import android.widget.TextView;
@@ -11,6 +15,8 @@ package com.example.admin.myapplication;
         import org.json.JSONObject;
 
         import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
 
         import okhttp3.Call;
         import okhttp3.Callback;
@@ -18,7 +24,9 @@ package com.example.admin.myapplication;
 
 public class Main2Activity extends AppCompatActivity {
 
-    private String[] data={"北京","浙江","安徽"};
+    private List<String> data2=new ArrayList();
+    private int[] pids=new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    private String[] data={"","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",};
     private TextView textView;
     private ListView listview;
 
@@ -31,6 +39,15 @@ public class Main2Activity extends AppCompatActivity {
         this.listview=(ListView)findViewById(R.id.listview);
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
         listview.setAdapter(adapter);
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("点击",""+position+":"+Main2Activity.this.pids[position]+":"+Main2Activity.this.data[position]);
+                Intent intent=new Intent(Main2Activity.this,MainActivity.class);
+                intent.putExtra("pid",Main2Activity.this.pids[position]);
+                startActivity(intent);
+            }
+        });
 
 //        String weatherId="CN101020200";
         String weatherUrl = "http://guolin.tech/api/china";
@@ -43,7 +60,7 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
-                String[] result=parseJSONObject(responseText);
+                parseJSONObject(responseText);
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -55,19 +72,18 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
-    private String[] parseJSONObject(String responseText) {
+    private void parseJSONObject(String responseText) {
         JSONArray jsonArray= null;
         try {
             jsonArray = new JSONArray(responseText);
-            String[] result=new String[jsonArray.length()];
             for (int i=0;i<jsonArray.length();i++){
                 JSONObject jsonObject=jsonArray.getJSONObject(i);
-                result[i]=jsonObject.getString("name");
+                jsonObject.getString("name");
+                this.data[i]=jsonObject.getString("name");
+                this.pids[i]=jsonObject.getInt("id");
             }
-            return result;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return null;
     }
 }
